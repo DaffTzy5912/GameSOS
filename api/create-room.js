b129f53f14c792dd9240d2fb46b2380d2c0fb848
DaffTzy5912 <dafanation999@gmail.com>
2025-03-29 22:28:31 +0700

    Update create-room.js

--- api/create-room.js
+++ api/create-room.js
@@ -1,10 +1,3 @@
-import { rooms } from "./server"; // Pastikan ini sesuai dengan struktur proyek
 
-export default function handler(req, res) {
-    let roomId = Math.random().toString(36).substring(7);
-    
-    // Simpan room di server
-    rooms[roomId] = { board: Array(9).fill(null), players: [], turn: 0 };
-    
-    res.json({ roomId });
-}
+export default function handler(req, res) { let roomId = Math.random().toString(36).substring(7); res.json({ roomId }); }
+
